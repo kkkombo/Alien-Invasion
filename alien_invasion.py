ebefe76dd1f472e47e517544c9002e2ebae4005a
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-03-15 22:24:28 +0100

    Add remove/update bullets method
    Resolve #11

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,7 +32,7 @@ class AlienInvasion:
             # Updating all this stuff with every loop 
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             
     def _check_events(self):
@@ -66,9 +66,20 @@ class AlienInvasion:
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
-        new_bullet = Bullet(self)
-        # Add to a group of bullet with .add (from Sprite)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            # Add to a group of bullet with .add (from Sprite)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions.
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared.
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
