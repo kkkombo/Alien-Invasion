683d2ea4bc9494bcf8bdda0bf70dcb6fb1dac37b
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-03-18 21:49:21 +0100

    Reset statistics and placing new items for new game

--- alien_invasion.py
+++ alien_invasion.py
@@ -117,7 +117,17 @@ class AlienInvasion:
     def _check_play_button(self, mouse_pos):
         """Start a new game when the player clicks Play."""
         if self.play_button.rect.collidepoint(mouse_pos):
+            # Reset game statistics before starting new game
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens annd bullets
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and the ship, place them
+            self._create_fleet()
+            self.ship.center_ship()
 
     def _fire_bullet(self):
         """Create a new bullet and add it to the bullets group."""
