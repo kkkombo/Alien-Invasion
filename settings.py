e2a1f748440eaefa211f51e31824c78ef3d5ea4f
Alexandra Dedok <dedok.alexandra@gmail.com>
2022-03-19 05:04:23 +0100

    Adjust alien speed
    Resolve #34

--- settings.py
+++ settings.py
@@ -34,7 +34,7 @@ class Settings:
         """Initialize settings that change throughout the game."""
         self.ship_speed = 1
         self.bullet_speed = 3.0
-        self.alien_speed = 2
+        self.alien_speed = 0.3
  
         
         # fleet_direction of 1 represents right; -1 represents left.
